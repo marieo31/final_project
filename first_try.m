e33ca8b35fbec05438953f0362ef954d36caf23b
MariO <40508635+marieo31@users.noreply.github.com>
2019-01-30 22:07:43 -0700

    data ok with translation transform

--- first_try.m
+++ first_try.m
@@ -5,21 +5,26 @@ clc
 nbpix = 4;
 
 % input matrix: Mr
-Mreal = ones(nbpix)+diag(1:nbpix);
+Mreal = ones(nbpix)+diag(1:nbpix)
 % to find the transforms, we need it to be invertible
 det(Mreal)
 
-% simulated output
+% simulated output --> translation of one row to the left
 Mmang = [Mreal(:,2:end), Mreal(:,1)]
 
 % Mangling matrix
-P = round(Mreal\Mmang,10)
+P = round(Mreal\Mmang*10^10)./10^10
 
 % Pre-processing matrix
 Pstar = inv(P)
 
+% Pre-processed image
+Mstar = Mreal*Pstar
 
-P2 = R/M
+% Simulation of vision with pre-processed image
+Result = Mstar*P
+
+% P2 = R/M
 
 P2*M
 
---
+++ generating_some_data.m
@@ -0,0 +1,48 @@
+clear variables
+format compact
+clc
+
+nbpix = 4;
+
+% Transformation matrices
+% translation to the left
+P = [   0     0     0     1;
+        1     0     0     0;
+        0     1     0     0;
+        0     0     1     0];
+% translation to the right
+Pstar = [    0     1     0     0;
+             0     0     1     0;
+             0     0     0     1;
+             1     0     0     0];
+
+
+% Define the vector to randomize the input matrix with different rates of 1
+% and 0
+s = 2^(-53);
+cut = [0.3:0.1:0.7].*(1-s)+s/2;
+
+nb_per_cut = 10000;
+tab_Mreal = zeros(nb_per_cut*length(cut),nbpix^2);
+tab_Mmang = zeros(nb_per_cut*length(cut),nbpix^2);
+tab_Mstar = zeros(nb_per_cut*length(cut),nbpix^2);
+
+jj = 1;
+for cc = 1:length(cut)
+    ii = 1;
+    while ii<nb_per_cut
+
+        Mreal = double((rand(4,4) > cut(cc)));        
+        Mmang = Mreal*P;
+        Mstar = Mreal*P;                
+        
+        tab_Mreal(jj,:) = reshape(Mreal, 1, nbpix^2);
+        tab_Mmang(jj,:) = reshape(Mmang, 1, nbpix^2);
+        tab_Mstar(jj,:) = reshape(Mmang, 1, nbpix^2);
+        
+        ii = ii+1;
+        jj = jj+1;
+    end
+end
+    
+    
