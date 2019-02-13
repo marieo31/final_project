5610c45cb80be2e1a3b98ddab8434e7c4609ef12
MariO <40508635+marieo31@users.noreply.github.com>
2019-02-12 17:18:32 -0700

    removing the debug mode

--- webapp/app.py
+++ webapp/app.py
@@ -207,4 +207,4 @@ def applyModel(nbpixTransformMat):
 #-------------------------------------------------------------------
 
 if __name__ == "__main__":
-    app.run(debug=True)    
+    app.run(debug=False)    
